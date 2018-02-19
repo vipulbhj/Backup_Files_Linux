1430c8aa36045e67adb5f22bfd3b67bf17747b47
vipulbhj <vipulsworkmail@gmail.com>
2018-02-19 23:32:28 +0530

    Some changes

--- Backup.sh
+++ Backup.sh
@@ -1,17 +1,29 @@
+#!/bin/bash
+
+
 # Checks if some arguments are passed to the program.
 if [ $# -gt 0 ]; then 
         # Checks each flag and based on the flag
         # Gives a appropriate response
         case "$1" in
-                -s)
-                        export source_prog=$2
+                -s)     
+                        source_prog=$2
+                        touch /home/vipul/Desktop .__source
+                        echo $source_prog > /home/vipul/Desktop.__source
+                        cat /home/vipul/Desktop.__source
                         ;;
                 -d)
-                        export destination_prog=$2
-                        mkdir -p $destination_prog
+                        des_prog=$2
+                        mkdir -p $des_prog
+                        touch /home/vipul/Desktop .__des
+                        echo $des_prog > /home/vipul/Desktop.__des
+                        cat /home/vipul/Desktop.__des
                         ;;
                 -r)
-                        readme_loc="$destination_prog/README.txt"
+                        source=$(</home/vipul/Desktop.__source)
+                        des=$(<//home/vipul/Desktop.__des)
+                        echo $source,$des
+                        readme_loc="$des/README.txt"
                         touch $readme_loc
                         # User that ran the script
                         name="$USER"
@@ -20,7 +32,7 @@ if [ $# -gt 0 ]; then
                         echo "Username:-  $name" >> $readme_loc
                         echo "Time:- $time" >> $readme_loc
                         echo "Source Directory:- $source_prog" >> $readme_loc
-                        echo "Backup Directory:- $destination_prog" >> $readme_loc
+                        echo "Backup Directory:- $des_prog" >> $readme_loc
                         ;;   
                 *)
                         echo "no such flag"
@@ -28,11 +40,14 @@ if [ $# -gt 0 ]; then
                         ;;                   
         esac
 else
-    # "No parameters, Actual backup job"
+    source=$(</home/vipul/Desktop.__source)
+    des=$(<//home/vipul/Desktop.__des)
+    # echo "No parameters, Actual backup job"
     # Find the base directory from -s address
-    source_dir=$(basename $source_prog)
+    echo $source,$des
+    source_dir=$(basename $source)
     # Making directory inside backup of the name sourcedirectory.
-    backup_source="$destination_prog/$source_dir"
+    backup_source="$des/$source_dir"
     # using three file addresses for {.1,.2,.3} directories.
     file_1="$backup_source.1"
     file_2="$backup_source.2"
@@ -45,13 +60,13 @@ else
     # Check if .2 exists
     if [ -d $file_2 ]; then
         # Delete the file
-        mv $file_2 $file_3
+        mv -v $file_2 $file_3
     fi
     # Check if .1 exists
     if [ -d $file_1 ]; then
         # Delete the file
-        mv $file_1 $file_2
+        mv -v $file_1 $file_2
     fi
     # copy sourcedir current state into .1
-    cp -R $source_prog $file_1
+    cp -R $source $file_1
 fi
